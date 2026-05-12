e4bf60a6361fbff1444a5c0819bbffd0ba0f4c97
Deepak-gowda11 <deepakgowda.22ise@cambridge.edu.in>
2026-05-12 15:55:15 +0530

    "Initial commit: Madhu-Marga Beekeeping App complete".

--- app/src/main/java/com/example/androidappdevelopmentusinggenai_madhu_margaagriculture/MainActivity.java
+++ app/src/main/java/com/example/androidappdevelopmentusinggenai_madhu_margaagriculture/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
